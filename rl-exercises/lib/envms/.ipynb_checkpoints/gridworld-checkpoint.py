fd060d384798166bc928d7189fdcc8a452c6162e
makaveli10 <suryanvineet47@gmail.com>
2020-03-24 01:28:02 +0530

    WIP: Jacks rental problem

---
+++ rl-exercises/lib/envms/.ipynb_checkpoints/gridworld-checkpoint.py
@@ -0,0 +1,16 @@
+class GridworldEnv(discrete.DiscreteEnv):
+    """
+    Grid World environment from Sutton's Reinforcement Learning book chapter 4.
+    You are an agent on an MxN grid and your goal is to reach the terminal
+    state at the top left or the bottom right corner.
+    For example, a 4x4 grid looks as follows:
+    T  o  o  o
+    o  x  o  o
+    o  o  o  o
+    o  o  o  T
+    x is your position and T are the two terminal states.
+    You can take actions in each direction (UP=0, RIGHT=1, DOWN=2, LEFT=3).
+    Actions going off the edge leave you in your current state.
+    You receive a reward of -1 at each step until you reach a terminal state.
+    """
+    pass
