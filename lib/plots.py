704e6d9b94b692410ab7f8f1bbc2ed0edef230f3
makaveli10 <suryanvineet47@gmail.com>
2020-04-17 21:02:05 +0530

    added: pot for mountain car

--- lib/plots.py
+++ lib/plots.py
@@ -10,6 +10,26 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 EpisodeStats = namedtuple("Stats",["episode_lengths", "episode_rewards"])
 
 
+def plot_cost_to_go_mountain_car(env, estimator, num_tiles=20, file=None):
+    x = np.linspace(env.observation_space.low[0], env.observation_space.high[0], num=num_tiles)
+    y = np.linspace(env.observation_space.low[1], env.observation_space.high[1], num=num_tiles)
+    X, Y = np.meshgrid(x, y)
+    Z = np.apply_along_axis(lambda _: -np.max(estimator.predict(_)), 2, np.dstack([X, Y]))
+
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_subplot(111, projection='3d')
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                           cmap=matplotlib.cm.coolwarm, vmin=-1.0, vmax=1.0)
+    ax.set_xlabel('Position')
+    ax.set_ylabel('Velocity')
+    ax.set_zlabel('Value')
+    ax.set_title("Mountain \"Cost To Go\" Function")
+    fig.colorbar(surf)
+    if file is None:
+        plt.show()
+    else:
+        plt.savefig(file+'Mountain_cost.png')
+
 
 def plot_value_function(V, title="Value Function"):
     """
