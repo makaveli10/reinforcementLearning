f3b9d730e2391c4ef7d3cbf489f6425b898a3d9b
makaveli10 <suryanvineet47@gmail.com>
2020-04-25 02:10:00 +0530

    added: pytorch double dqn

--- DQN/pytorch/agents.py
+++ DQN/pytorch/agents.py
@@ -12,7 +12,6 @@ from replay_memory import ReplayMemory
 from q_networks import DQNEstimator
 
 
-
 class DQNAgent():
     def __init__(self, state_size, action_size):
         self.state_size = state_size
@@ -82,7 +81,8 @@ class DQNAgent():
         is_dones = torch.FloatTensor(is_dones.astype(int))
         
         # forward propagation Q_network for current states
-        states = torch.Tensor(states).float()
+        states = torch.Tensor(states)
+        states = Variable(states).float()
         preds = self.q_estimator(states)
         
         # onehot encoding actions
@@ -108,3 +108,97 @@ class DQNAgent():
         loss.backward()
         self.optimizer.step()
         return loss.item()
+    
+
+class DoubleDQNAgent():
+    def __init__(self, state_size, action_size):
+        # for rendering
+        self.render = False
+        
+        self.state_size = state_size
+        self.action_size = action_size
+        
+        # hyperparams for estimator
+        self.gamma = 0.95
+        self.lr = 0.001
+        self.replay_memory_size = 50000
+        self.epsilon = 1.0
+        self.epsilon_min = 0.000001
+        self.explore_steps = 3000
+        self.epsilon_decay = (self.epsilon - self.epsilon_min) / self.explore_steps
+        self.batch_size = 32
+        self.replay_memory_init_size = 1000
+        
+        # Estimators
+        self.q_estimator = DQNEstimator(state_size, action_size)
+        self.target_estimator = DQNEstimator(state_size, action_size)
+        self.optimizer = optim.SGD(self.q_estimator.parameters(), lr=self.lr)
+        
+        # memory 
+        self.memory = ReplayMemory(self.replay_memory_size)
+        
+    def update_target_estimator(self):
+        self.target_estimator.load_state_dict(self.q_estimator.state_dict())
+    
+    def get_action(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        else:
+            state = Variable(torch.from_numpy(state)).float()
+            q_values = self.q_estimator(state)
+            _, best_action = torch.max(q_values, dim=1)
+            return int(best_action)
+    
+    def train(self):
+        # epsilon decay
+        if self.epsilon > self.epsilon_min:
+            self.epsilon -= self.epsilon_decay
+        # fetch samples from memory
+        batch = self.memory.sample(self.batch_size)
+        batch = np.array(batch).transpose()
+        
+        # stack all the states
+        states = np.vstack(batch[0])
+        actions = torch.LongTensor(list(batch[1]))
+        rewards = torch.FloatTensor(list(batch[2]))
+        
+        # stack all the next states
+        next_states = np.vstack(batch[3])
+        dones = batch[4]
+        dones = dones.astype(int)
+        
+        # actions one hot encoding
+        actions_one_hot = F.one_hot(actions, num_classes=self.action_size)
+        actions_one_hot = torch.FloatTensor(actions_one_hot.float())
+        actions_one_hot = Variable(actions_one_hot)
+        
+        # Forward prop
+        states = torch.FloatTensor(states)
+        states = Variable(states)
+        preds = self.q_estimator(states)
+        
+        # get current action value
+        preds = torch.sum(torch.mul(preds, actions_one_hot), dim=1)
+        
+        # Double DQN
+        next_states = torch.FloatTensor(next_states)
+        next_states = Variable(next_states)
+        next_action_values = self.q_estimator(next_states)
+        best_actions = torch.argmax(next_action_values, dim=1)
+        q_values_next_target = self.target_estimator(next_states)
+
+        dones = torch.FloatTensor(dones)
+        target = rewards + (1 - dones) * self.gamma * q_values_next_target[np.arange(self.batch_size), 
+                                                                           best_actions]
+        target = Variable(target)
+        
+        loss = F.mse_loss(preds, target).mean()
+        
+        # zero out accumulated grads
+        self.optimizer.zero_grad()
+        
+        # back prop
+        loss.backward()
+        self.optimizer.step()
+        return loss.item()
+        
--- DQN/pytorch/double_dqn.py
+++ DQN/pytorch/double_dqn.py
@@ -8,38 +8,38 @@ import matplotlib
 import matplotlib.pyplot as plt
 from itertools import count
 from PIL import Image
-from agents import DQNAgent
-matplotlib.use('Agg')
-matplotlib.style.use('ggplot')
+from agents import DoubleDQNAgent
 
 
 env = gym.make('CartPole-v0')
-
 if __name__=="__main__":
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
-    agent = DQNAgent(state_size, action_size)
+    agent = DoubleDQNAgent(state_size, action_size)
     scores, episodes = list(), list()
     num_episodes = 500
     total_steps = 0
+    flag = False
     state = env.reset()
     state = np.reshape(state, [1, state_size])
-    flag = False
-    # populate replay memory
     score = 0
+
+    # Populate replay memory
+    print("Populating replay memory")
     for i in range(agent.replay_memory_init_size):
         action = agent.get_action(state)
-        next_state, reward, done, _ = env.step(action)
+        next_state, reward, is_done, _ = env.step(action)
         next_state = np.reshape(next_state, [1, state_size])
-        reward = reward if not done or score == 200 else -10
+        reward = reward if not is_done or score == 200 else -10
+        agent.memory.push(state, action, reward, next_state, is_done)
         score += reward
-        agent.memory.push(state, action, reward, next_state, done)
-        if done:
+        if is_done:
             state = env.reset()
             state = np.reshape(state, [1, state_size])
             score = 0
         else:
             state = next_state
+
     for i_episode in range(1, num_episodes + 1):
         score = 0
         # update target network
@@ -54,7 +54,7 @@ if __name__=="__main__":
             next_state = np.reshape(next_state, [1, state_size])
             reward = reward if not done or score == 199 else -10
             agent.memory.push(state, action, reward, next_state, done)
-            loss = agent.train_network()
+            loss = agent.train()
             score += reward
             state = next_state
             if done:
@@ -64,9 +64,9 @@ if __name__=="__main__":
                 plt.plot(episodes, scores, label="Scores vs Episode")
                 plt.xlabel("Episodes")
                 plt.ylabel("Score")
-                plt.savefig("plots/cartpole_dqn.png")
+                plt.savefig("plots/cartpole_double_dqn.png")
                 if np.mean(scores[-min(100, len(scores)):]) >=195:
-                    torch.save(agent.q_estimator.state_dict(), "saved_model/cartpole_dqn")
+                    torch.save(agent.q_estimator.state_dict(), "saved_model/cartpole_double_dqn")
                     print("Saved Model")
                     print("Average Score of last 100 episodes {}".format(np.mean(scores[-min(100, len(scores)):])))
                     flag = True
@@ -75,6 +75,3 @@ if __name__=="__main__":
             total_steps += 1
         print("\rTotal Steps: {}  Episode {}/{} Loss: {:.4f}  epsilon: {}".format(total_steps, i_episode, num_episodes, loss, agent.epsilon), end="")
         sys.stdout.flush()
-    if not flag:
-        torch.save(agent.q_estimator.state_dict(), "saved_model/cartpole_dqn")
-        print("Saved model")
